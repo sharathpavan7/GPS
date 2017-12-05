235e9c467ea64d770325519b9f2050ab1542fc8c
YakseeHyd <suresh@yaksee.com>
2017-12-05 21:50:14 +0530

    New file is added for handling GPS settings & all.

---
+++ app/src/main/java/com/sharath/gps/GPSTracker.java
@@ -0,0 +1,177 @@
+package com.sharath.gps;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by yaksee on 12/5/17.
+ */
+
+public class GPSTracker extends Service implements LocationListener {
+
+    private final Context mcontext;
+
+    boolean isGPSEnabled = false;
+
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    Location location;
+    double latitude;
+    double langitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1MIN (Milli sec * sec * min)
+
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context) {
+        mcontext = context;
+        getLocation();
+    }
+
+    public Location getLocation() {
+
+        locationManager = (LocationManager) mcontext.getSystemService(LOCATION_SERVICE);
+
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnabled && !isNetworkEnabled) {
+
+        } else {
+            this.canGetLocation = true;
+
+            if (isNetworkEnabled) {
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity)mcontext,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET},
+                            0);
+                    return null;
+                }
+
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                    if (location != null) {
+                        langitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                    }
+                }
+            }
+
+            if (isGPSEnabled) {
+                if (location == null) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+
+                    if (locationManager != null){
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                        if (location != null){
+                            latitude = location.getLatitude();
+                            langitude = location.getLongitude();
+
+                        }
+                    }
+                }
+            }
+        }
+
+        return location;
+    }
+
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    public double getLatitude(){
+        if (location != null){
+            latitude = location.getLatitude();
+        }
+
+        return latitude;
+    }
+
+    public double getLangitude() {
+        if (location != null){
+            langitude = location.getLongitude();
+        }
+
+        return langitude;
+    }
+
+    public boolean canGetLocation() {
+        return canGetLocation;
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mcontext);
+        alertDialog.setTitle("GPS settings");
+        alertDialog.setMessage("GPS is not enabled . Do you want to go to settings?");
+
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mcontext.startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+}
